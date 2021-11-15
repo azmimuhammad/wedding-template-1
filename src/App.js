ae9c1cc24b4cddc5e2932d8f8d8641b0a71e6c89
Azmi Muhammad <azmim85@gmail.com>
2021-11-16 04:05:17 +0700

    fix: wording dialog

--- src/App.js
+++ src/App.js
@@ -18,9 +18,10 @@ function App() {
 
   return (
     <div className="App">
-      <audio id="backsound" hidden autoplay loop>
+      <audio id="backsound" autoplay loop>
         <source src={Backsound} type="audio/mpeg" />
       </audio>
+      {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/j-ZGEx5Gxgo?controls=0&amp;start=9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
       {!open && <HomePage />}
       <DialogComponent open={open} handleClose={handleClose} />
     </div>
