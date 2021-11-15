2433e9fc60dd63bc463e94cef2244d16084d39c1
Azmi Muhammad <azmim85@gmail.com>
2021-11-16 03:23:26 +0700

    coba audio

--- src/App.js
+++ src/App.js
@@ -2,31 +2,25 @@ import React, { useState } from 'react';
 import './App.css';
 import HomePage from './pages';
 import DialogComponent from './pages/components/DialogOpen'
-// import Backsound from './assets/backsound.mp3'
+import Backsound from './assets/backsound.mp3'
 
 function App() {
   const [open, setOpen] = useState(true)
   // const [audio] = useState(new Audio(Backsound))
 
   const handleClose = () => {
-    // const x = document.getElementById("backsound")
+    const x = document.getElementById("backsound")
 
-    // audio.play()
+    x.play()
     // audio.addEventListener('ended', () => audio.play());
     setOpen(false)
   }
 
   return (
     <div className="App">
-      {/* <audio id="backsound" src={Backsound} controls> */}
-      {/* <embed
-        src={Backsound}
-        width="300"
-        height="90" /> */}
-      {/* </audio> */}
-      {/* <audio id="backsound" autoPlay={!open} loop>
-        <source src={Backsound} />
-      </audio> */}
+      <audio id="backsound" hidden autoplay loop>
+        <source src={Backsound} type="audio/mpeg" />
+      </audio>
       {!open && <HomePage />}
       <DialogComponent open={open} handleClose={handleClose} />
     </div>
